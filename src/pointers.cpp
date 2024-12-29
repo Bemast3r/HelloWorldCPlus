7c45b18c1bcddd6b1202a2dec2f12f9bf5aeae37
Bemast3r <dahempvp@gmail.com>
2024-12-29 21:38:54 +0100

    Pointers verstanden.

---
+++ src/pointers.cpp
@@ -0,0 +1,19 @@
+void pointer() {
+	void* ptr = NULL;
+	// void* ptr = NULL;
+	// void* ptr = 0;
+	// void* ptr = nullptr;
+	int var = 8;
+	int* ptrs = &var;
+	// Auf die Datei zugreifen
+	*ptrs = 10;
+	var = 19;
+	std::cout << var << std::endl;
+	// buffer ist eine adresse
+	char* buffer = new char[8];
+	memset(buffer, 0, 8);
+	char** ptbuf = &buffer;
+
+	delete[] buffer;
+	std::cin.get();
+}

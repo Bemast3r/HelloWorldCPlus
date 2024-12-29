e4bb24d111250115c3f68c6b384b86fd7141fcf6
Bemast3r <dahempvp@gmail.com>
2024-12-29 20:05:22 +0100

    For loops

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,27 +10,10 @@ int Multiply(int a, int b);
 
 int main() 
 {
-	int a = 8;
-	a++;
-	const char* string = "Hello";
 
-
-	int condition = 8;
-	bool comparision = condition == 5;
-
- 	if (comparision) {
-		Log("Ist True");
-		return 0;
-	}
-	else if(condition == 8){
-		Log("8");
-	}
-	else {
-		Log("Nicht true");
+	for (int i = 0; i < 5; i++) {
+		Log("hello World");
 	}
 
-	Log("New Worlds");
-	int res = Multiply(5, 8);
-	std::cout << res << std::endl;
 	std::cin.get();
 } 
